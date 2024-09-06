280651ed82223c153f6a473596b19643acd3f12f
Saimun-jd <saimuncoder@gmail.com>
2024-09-06 22:45:25 +0600

    game screen restricted to desktop device

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,16 @@
 import OceanGuardian from "./OceanGuardianGame"
-
+import useCheckMobileScreen from "../hooks/useCheckMobileScreen";
 
 const Game = () => {
+  const isMobile = useCheckMobileScreen();
+
   return (
-    <div className="app">
+    <div className="app relative">
+      {
+        isMobile && (<div className="fixed inset-0 bg-black bg-opacity-50 text-white font-bold z-50 flex justify-center items-center text-center">
+          <span>Please open desktop window for playing the game</span>
+        </div>)
+      }
         <OceanGuardian/>
     </div>
 
