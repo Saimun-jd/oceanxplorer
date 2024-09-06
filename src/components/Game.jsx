1caf9d4dca1190bf56761e04c4b093158f43c5e0
Saimun-jd <saimuncoder@gmail.com>
2024-09-06 22:52:23 +0600

    added home navigation to game screen if not desktop

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,6 @@
 import OceanGuardian from "./OceanGuardianGame"
 import useCheckMobileScreen from "../hooks/useCheckMobileScreen";
+import { NavLink } from "react-router-dom";
 
 const Game = () => {
   const isMobile = useCheckMobileScreen();
@@ -7,8 +8,9 @@ const Game = () => {
   return (
     <div className="app relative">
       {
-        isMobile && (<div className="fixed inset-0 bg-black bg-opacity-50 text-white font-bold z-50 flex justify-center items-center text-center">
+        isMobile && (<div className="fixed inset-0 bg-black bg-opacity-50 text-white font-bold z-50 flex flex-col justify-center items-center text-center">
           <span>Please open desktop window for playing the game</span>
+          <NavLink to="/" className="underline text-blue-500">Home</NavLink>
         </div>)
       }
         <OceanGuardian/>
